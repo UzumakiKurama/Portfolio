2281532869cf336bf82e5d2e6f616276d08f33d0
UzumakiKurama <bbgiabhijeet12@gmail.com>
2022-02-06 12:08:14 +0530

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,46 @@
+const itemCallback = (entries) => {
+     entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+               entry.target.classList.add("animate");
+               entry.target.classList.add("blur");
+          }
+     });
+};
+const itemOptions = {
+     rootMargin: "-100px",
+};
+
+// Tools animations
+const toolsHeading = document.getElementsByClassName("tools__heading");
+const toolsListitem = document.getElementsByClassName("tools__list--item");
+
+const toolsHeading_observer = new IntersectionObserver(
+     itemCallback,
+     itemOptions
+);
+const toolsListItem_observer = new IntersectionObserver(
+     itemCallback,
+     itemOptions
+);
+
+for (let i = 0; i < toolsHeading.length; i++) {
+     toolsHeading_observer.observe(toolsHeading[i]);
+}
+
+for (let i = 0; i < toolsListitem.length; i++) {
+     toolsListItem_observer.observe(toolsListitem[i]);
+}
+
+// Project animations
+
+const item1 = document.getElementById("project-1");
+const item2 = document.getElementById("project-2");
+const item3 = document.getElementById("project-3");
+
+const item1Observer = new IntersectionObserver(itemCallback, itemOptions);
+const item2Observer = new IntersectionObserver(itemCallback, itemOptions);
+const item3Observer = new IntersectionObserver(itemCallback, itemOptions);
+
+item1Observer.observe(item1);
+item2Observer.observe(item2);
+item3Observer.observe(item3);
