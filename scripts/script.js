fe1e53444baf3f0f587026efe0b244f3aa0c783c
UzumakiKurama <bbgiabhijeet12@gmail.com>
2024-08-14 08:58:24 +0530

    Added a new project

--- scripts/script.js
+++ scripts/script.js
@@ -36,11 +36,14 @@ for (let i = 0; i < toolsListitem.length; i++) {
 const item1 = document.getElementById("project-1");
 const item2 = document.getElementById("project-2");
 const item3 = document.getElementById("project-3");
+const item4 = document.getElementById("project-4");
 
 const item1Observer = new IntersectionObserver(itemCallback, itemOptions);
 const item2Observer = new IntersectionObserver(itemCallback, itemOptions);
 const item3Observer = new IntersectionObserver(itemCallback, itemOptions);
+const item4Observer = new IntersectionObserver(itemCallback, itemOptions);
 
 item1Observer.observe(item1);
 item2Observer.observe(item2);
 item3Observer.observe(item3);
+item4Observer.observe(item4);
